537e416e5183d6376f8bd26ddd9da4bada0c0e02
George  Fu-Cheng Pan <georgepan900530@gmail.com>
2024-11-29 11:21:49 -0500

    Add eval.py

--- eval.py
+++ eval.py
@@ -1,3 +1,4 @@
+from ast import arg
 from cleanfid import fid
 import cv2
 import os
@@ -11,9 +12,26 @@ from scipy.special import gamma
 from scipy.ndimage.filters import gaussian_filter
 import scipy.io
 import torch
+import argparse
 
 
-real_folder = ""
-fake_folder = ""
+def get_parser():
+    parser = argparse.ArgumentParser(description="Evaluation")
+    parser.add_argument(
+        "--real_folder", type=str, required=True, help="Path to the real image folder"
+    )
+    parser.add_argument(
+        "--fake_folder", type=str, required=True, help="Path to the fake image folder"
+    )
+    return parser
+
+
+parser = get_parser()
+args = parser.parse_args()
+
+
+real_folder = args.real_folder
+fake_folder = args.fake_folder
 
 score = fid.compute_fid(real_folder, fake_folder)
+print(f"FID score: {score:.5f}")
